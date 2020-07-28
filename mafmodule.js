8d5a64f93dd537c9707009dad0f84850560f59fc
maf <61792345+mafgit@users.noreply.github.com>
2020-07-29 00:21:08 +0500

    Update mafmodule.js

--- mafmodule.js
+++ mafmodule.js
@@ -1,10 +1,5 @@
-// Remove element from an array
 const remove = (arr, index) => arr.filter((el) => arr.indexOf(el) !== index);
-// let arr = [0,1,2,3]
-// arr = remove(arr, 2)
-// console.log(arr)
 
-// Shuffle an array
 const shuffle = (arr) => {
   let newArr = [];
   let indices = [];
@@ -17,11 +12,16 @@ const shuffle = (arr) => {
   }
   return newArr;
 };
-// console.log(shuffle([1, 2, 2, 2, 2, 3, 4, 4]))
 
-// Count number of duplicates of an element in an array
 const count = (arr, el) => {
   return arr.filter((arrEl) => arrEl === el).length;
 };
-// let arr = [1,2,3,2,2,'two'];
-// console.log(count(arr, 2))
+
+function range(start, end, steps = 1) {
+  let arr = []
+  steps = steps === 0 ? 1 : steps
+  for (let i = start; i < end; i += steps) {
+    arr.push(i)
+  }
+  return arr
+}
